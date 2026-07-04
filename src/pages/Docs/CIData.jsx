a94380e75dbfe758c5dd2d484fd74a8d227e7746
etzba <supoort@etzba.com>
2026-07-04 14:55:05 +0200

    Update docs

---
+++ src/pages/Docs/CIData.jsx
@@ -0,0 +1,36 @@
+import "../../styles/layout.css";
+
+const CIIntegration = () => {
+  return (
+    <div className="container">
+      <section className="section">
+        <h1>CIIntegration</h1>
+        <p>Content is in progress</p>
+      </section>
+    </div>
+  );
+};
+
+const CreateArgoWorkflowTemplate = () => {
+  return (
+    <div className="container">
+      <section className="section">
+        <h1>CreateArgoWorkflowTemplate</h1>
+        <p>Content is in progress</p>
+      </section>
+    </div>
+  );
+};
+
+const IntegrateInGitLabCI = () => {
+  return (
+    <div className="container">
+      <section className="section">
+        <h1>IntegrateInGitLabCI</h1>
+        <p>Content is in progress</p>
+      </section>
+    </div>
+  );
+};
+
+export { CIIntegration, CreateArgoWorkflowTemplate, IntegrateInGitLabCI };
--- src/routes/docsItems.jsx
+++ src/routes/docsItems.jsx
@@ -1,10 +1,18 @@
-import { GeneralConfig, RunWithExecutionFile, TestCases } from "../pages/Docs/ConfigurationData";
+import { AdvancedConfiguration, UseGoTemplates } from "../pages/Docs/AdvancedData";
+import { CIIntegration, CreateArgoWorkflowTemplate, IntegrateInGitLabCI } from "../pages/Docs/CIData";
+import {
+  GeneralConfig,
+  Commands,
+  RunWithExecutionFile,
+  TestCases,
+} from "../pages/Docs/ConfigurationData";
 import {
   GettingStarted,
+  InstallEtz,
   RunEtz,
   SimpleExec,
 } from "../pages/Docs/GettingStartedData";
-import { Install, InstallLinux, InstallMacOS } from "../pages/Docs/InstallData";
+import { RunLoadTests, RunWithDocker } from "../pages/Docs/RunData";
 import Welcome from "../pages/Docs/WelcomeData";
 
 export const DocsItems = [
@@ -17,35 +25,19 @@ export const DocsItems = [
     exact: true,
   },
   {
-    id: "install",
-    title: "Install",
-    path: "install",
-    label: "Install",
-    element: <Install />,
-    children: [
-      {
-        id: "linux",
-        title: "Install on Linux",
-        path: "linux",
-        label: "Linux",
-        element: <InstallLinux />,
-      },
-      {
-        id: "macos",
-        title: "Install on MacOS",
-        path: "osx",
-        label: "Mac",
-        element: <InstallMacOS />,
-      },
-    ],
-  },
-  {
     id: "start",
     title: "Getting started",
     path: "start",
     label: "Started",
     element: <GettingStarted />,
     children: [
+      {
+        id: "install",
+        title: "Install etz",
+        path: "install",
+        label: "Install",
+        element: <InstallEtz />,
+      },
       {
         id: "run",
         title: "Running etz from terminal",
@@ -70,18 +62,80 @@ export const DocsItems = [
     element: <TestCases />,
     children: [
       {
+        id: "commands",
+        title: "Available commands",
+        label: "Commands",
+        path: "commands",
+        element: <Commands />,
+      },
+      {
+        id: "exec",
+        title: "Configuring execution file",
+        label: "Execution",
+        path: "executions",
+        element: <RunWithExecutionFile />,
+      },
+      {
         id: "general",
         title: "Run etz with general configuration",
         label: "General",
         path: "general",
         element: <GeneralConfig />,
       },
+    ],
+  },
+  {
+    id: "advanced",
+    title: "Advanced Test Cases Configuration",
+    path: "advanced",
+    label: "Advanced",
+    element: <AdvancedConfiguration />,
+    children: [
       {
-        id: "exec",
-        title: "Run etz with execution file",
-        label: "Execution",
-        path: "executions",
-        element: <RunWithExecutionFile />,
+        id: "gotmpl",
+        title: "Use built-in go template functions",
+        label: "Gotmpl",
+        path: "gotmpl",
+        element: <UseGoTemplates />,
+      },
+    ],
+  },
+  {
+    id: "run",
+    title: "Run etz Load Tests",
+    path: "run",
+    label: "Run",
+    element: <RunLoadTests />,
+    children: [
+      {
+        id: "docker",
+        title: "Use etzba test servers with docker",
+        label: "Docker",
+        path: "docker",
+        element: <RunWithDocker />,
+      },
+    ],
+  },
+  {
+    id: "ci",
+    title: "CI integration",
+    path: "ci",
+    label: "CI",
+    element: <CIIntegration />,
+    children: [
+      {
+        id: "argo",
+        title: "Run etz as argo workflow",
+        label: "Argo",
+        path: "argo",
+        element: <CreateArgoWorkflowTemplate />,
+      },
+      {
+        id: "gitlab",
+        title: "Integrate etz in gitlab ci",
+        label: "Gitlab",
+        path: "gitlab",
+        element: <IntegrateInGitLabCI />,
       },
     ],
   },
