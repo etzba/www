56147586f62bd1a27b84660f7bebddd4b367df11
etzba <supoort@etzba.com>
2026-09-10 11:01:06 +0200

    styling about in mobile

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import InfoBlock from "../../components/InfoBlock";
+import "../../styles/about.css";
+import "../../styles/info.css";
 
 const AboutPage = () => {
   return (
     <>
-      <div className="docs">
+      <div className="about">
         <h1>About etzba</h1>
         <p>
-          Hi! My name is Nadav and I made this website and the products that
-          are provided here.
+          Hi! My name is Nadav and I made this website and the products that are
+          provided here.
         </p>
         <p>
           It all started with a load testing tool I created from curiosity, as
@@ -18,32 +20,33 @@ const AboutPage = () => {
           tools.{" "}
         </p>{" "}
         <InfoBlock
+          className="info"
           preText={
             "The word etzba in Hebrew, used as a measurement unit in biblical times and also means a pointing finger."
           }
         />
         <p>
           {" "}
-          All of the tools here aim to improve CI\CD (Continuous
-          Integration and Continuous Deployment) process and join in addition to
-          the current cycle of development and release, until it is shipped to
-          production and back to development.
+          All of the tools here aim to improve CI\CD (Continuous Integration and
+          Continuous Deployment) process and join in addition to the current
+          cycle of development and release, until it is shipped to production
+          and back to development.
         </p>
         <p>
           The first tool <code>etz</code> CLI, was meant to measure a service's
           performance and point a finger at issues that might arise when you
           increase the load on the application. I wanted to use a simple command
-          line tool with an option to add YAML files to any load test I'm running.
-          Every devops engineer, developer, QA or testing engineer can use it
-          easily.
+          line tool with an option to add YAML files to any load test I'm
+          running. Every devops engineer, developer, QA or testing engineer can
+          use it easily.
         </p>
         <p>
-          More devops tools were created to deal with some missing parts that require
-          automation inside kubernetes clusters, but it is also dedicated for
-          deploying <code>etz</code> CLI easily and creating a whole stack of
-          testing that fits right into CI. It was made to solve some issues I saw
-          while deploying to kubernetes in production or in testing
-          environments.
+          More devops tools were created to deal with some missing parts that
+          require automation inside kubernetes clusters, but it is also
+          dedicated for deploying <code>etz</code> CLI easily and creating a
+          whole stack of testing that fits right into CI. It was made to solve
+          some issues I saw while deploying to kubernetes in production or in
+          testing environments.
         </p>
         <p>
           If you find it interesting and would like to contact me for a fix,
@@ -51,6 +54,8 @@ const AboutPage = () => {
           environment, I would be happy to do so. Please write an email to
           support@etzba.com and keep in touch ;) .
         </p>
+        <br></br>
+        <br></br>
       </div>
     </>
   );
